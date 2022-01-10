f02158582e631eb3e24a0b1c25178d74dabe9457
Armando Andini <armando.andini@gmail.com>
2022-01-10 12:26:53 -0300

    Update comments on PUSH instructions

--- puzzles/puzzle_7.js
+++ puzzles/puzzle_7.js
@@ -4,7 +4,7 @@ const puzzleCode = [
   '36',     // CALLDATASIZE
   '6003',   // PUSH1 03
   '10',     // LT
-  '6009',   // PUSH1 0d
+  '6009',   // PUSH1 09
   '57',     // JUMPI
   'fdfd',   // REVERT REVERT
   '5b',     // JUMPDEST
@@ -13,7 +13,7 @@ const puzzleCode = [
   '02',     // MUL
   '6008',   // PUSH1 08
   '14',     // EQ
-  '6014',   // PUSH1 0d
+  '6014',   // PUSH1 14
   '57',     // JUMPI
   'fd',     // REVERT
   '5b',     // JUMPDEST
