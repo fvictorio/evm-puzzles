fd571052063d16b8170ca7d29b4e641c3d1c80c8
Franco Victorio <victorio.franco@gmail.com>
2022-03-05 00:12:02 -0300

    Show URL to evm.codes playground

--- puzzles/setup.js
+++ puzzles/setup.js
@@ -3,21 +3,35 @@ async function setup(puzzleCode, { data, value }) {
 
   const address = "0xffffffffffffffffffffffffffffffffffffffff";
 
-  await hre.network.provider.send("hardhat_setCode", [address, `0x${puzzleCode}`]);
+  await hre.network.provider.send("hardhat_setCode", [
+    address,
+    `0x${puzzleCode}`
+  ]);
 
-  data = data.startsWith("0x") ? data : `0x${data}`
+  data = data.startsWith("0x") ? data : `0x${data}`;
 
+  const evmCodesUrlData = data === "0x" ? "" : data;
+  const evmCodesUrl = `https://www.evm.codes/playground?callValue=${value}&unit=Wei&callData=${evmCodesUrlData}&codeType=Bytecode&code='${puzzleCode}'_`;
+
+  let failed = false;
   try {
     await s.sendTransaction({
       to: address,
       data,
       gasLimit: 1_000_000,
-      value,
+      value
     });
 
-    console.log('Puzzle solved!');
+    console.log("Puzzle solved!");
   } catch (e) {
-    console.error('Wrong solution')
+    console.error("Wrong solution :(");
+    failed = true;
+  }
+
+  console.log();
+  console.log("Run it in evm.codes:", evmCodesUrl);
+
+  if (failed) {
     process.exit(1);
   }
 }
